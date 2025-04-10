7995d5ee02989c66b0eed873d61387107ca3bf11
kimjunseok00 <proluver1321@gmail.com>
2025-04-10 12:03:52 +0900

    Title: 황금비율 토스트, Time: 16ms, Memory: 1MB, Status: Runtime Error - Codetree

---
+++ 250410/황금비율 토스트/golden-toast.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <list>
+using namespace std;
+
+int n, m;
+string s;
+
+int main() {
+    cin >> n >> m;
+    cin >> s;
+    list<char> l;
+    list<char>::iterator it;
+    
+    for(char c : s ){
+        l.push_back(c);
+    }
+    
+    it = l.end() ;
+    
+    for (int i = 0; i < m; i++) {
+        char command;
+        cin >> command;  
+        if (command == 'P'){ // 문자 추가
+            char c;
+            cin >> c;
+            l.insert(it,c) ;    
+        }
+        else if(command=='L') {// 바로 앞 위치변경
+            it--;
+        }
+        else if(command=='R'){ // 바로 뒤 위치변경
+            it++;
+        }
+        else if(command=='D'){ // 바로 뒤 제거 
+            l.erase(it);
+            it--;
+        }
+    }
+
+    for(it = l.begin(); it != l.end(); it++) { // 'a' 'b' 'c'
+        cout << *it ;                   // it가 가리키는 주소에 담긴 데이터 참조를 위해 * 사용
+    }  // Please write your code here.
+
+    return 0;
+}
