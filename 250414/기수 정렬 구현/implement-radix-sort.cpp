1306ba7ed8a0440b8c9d8f367e69671230713c80
kimjunseok00 <proluver1321@gmail.com>
2025-04-14 05:33:17 +0900

    Title: 기수 정렬 구현, Time: 15ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 250414/기수 정렬 구현/implement-radix-sort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[100];
+void bubble(int arr[], int k){
+    int max = arr[0] ;
+    
+    for(int i = 0 ; i < n ; i++){
+        if(max < arr[i]) max = arr[i] ;
+    }
+    
+    for (int exp = 1 ; max / exp > 0 ; exp *= 10){
+        int brr[10][100] = {0} ;
+        int brrsize[10] = {0} ;
+        
+        for(int j=0 ; j < k ; j++){
+            int num = (arr[j] / exp) % 10 ;
+            brr[num][brrsize[num]++] = arr[j] ;
+        }
+    int idx = 0 ; 
+    for (int i = 0 ; i<10 ; i++){
+        for(int j = 0 ; j<brrsize[i] ;j++){
+            arr[idx++] = brr[i][j] ;
+            }
+        }
+    }
+}
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    bubble(arr,n) ;
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
