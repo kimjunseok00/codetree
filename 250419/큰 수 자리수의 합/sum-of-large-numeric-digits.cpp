8a1cff9c397297b1841c0b2e358efb2c3419bd5d
kimjunseok00 <proluver1321@gmail.com>
2025-04-19 21:45:47 +0900

    Title: 큰 수 자리수의 합, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250419/큰 수 자리수의 합/sum-of-large-numeric-digits.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b, c;
+int arr[10] ;
+int f(int num){
+    if(num==0) return 0; 
+    return (num/100) + (num/10%10) + num%10 ;
+}
+
+int sum(int x, int y,int z){
+    if(x<1 && y<1 && z<1 ) return 0 ;
+    return sum(x/10 ,y/10,z/10) + f( (x%10) * (y%10)* (z%10)); 
+}
+int main() {
+    cin >> a >> b >> c;
+
+    cout << sum(a,b,c) ;
+
+    return 0;
+}
