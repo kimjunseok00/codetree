03c971c34798694de6353e05c97a9b224a7d139e
kimjunseok00 <proluver1321@gmail.com>
2025-04-19 22:04:11 +0900

    Title: 큰 수 자리수의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250419/큰 수 자리수의 합/sum-of-large-numeric-digits.cpp
+++ 250419/큰 수 자리수의 합/sum-of-large-numeric-digits.cpp
@@ -3,22 +3,19 @@
 using namespace std;
 
 int a, b, c;
-int arr[10] ;
-int f(int num){
+
+long long f(long longnum){
     if(num==0) return 0; 
     return (num%10) + f(num/10);
 }
 
-int sum(int x, int y,int z){
-    if(x<1 && y<1 && z<1 ) return 0 ;
-    
-    if(x<10&&y<10&&z<10) return f((x%10)*(y%10)*(z%10));
-    return sum(x/10 ,y/10,z/10) + f( (x%10) * (y%10)* (z%10)); 
-}
 int main() {
+    
     cin >> a >> b >> c;
 
-    cout << sum(a,b,c) ;
+    long long result = a*b*c; 
+    
+    cout << f(result) ;
 
     return 0;
 }
