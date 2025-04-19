677ab2f965a39f696013bb1a3a2ff06a2d7ff6eb
kimjunseok00 <proluver1321@gmail.com>
2025-04-19 16:02:03 +0900

    Title: 1이 되는 순간까지, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250419/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
+++ 250419/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
@@ -5,12 +5,12 @@ using namespace std;
 int N;
 
 int f(int a){
-    if(a<10) return a;
+    if(a<=1) return 0;
     
-    if((a/10)%2==0)
-    return f(a/10) + a%10 ;
+    if(a%2==0)
+    return f(a/2) + 1 ;
     else
-    return f(a/10) ;
+    return f(a/3) + 1;
 
 }
 int main() {
