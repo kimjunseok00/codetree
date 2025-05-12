c770a8327fd86b2e6f93a9b01224f1e656290189
kimjunseok00 <proluver1321@gmail.com>
2025-05-12 13:32:05 +0900

    Title: 정수 명령 처리, Time: 15ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250512/정수 명령 처리/process-numeric-commands.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int N;
+string command[10000];
+int value[10000];
+
+int main() {
+    cin >> N;
+    
+    for (int i = 0; i < N; i++) {
+        cin >> command[i];
+        if (command[i] == "push") {
+            cin >> value[i];
+        }
+    }
+    
+    stack<int> s;
+    
+    for(int i =0 ; i<N ; i++){
+        
+        if(command[i]=="push"){
+            s.push(value[i]) ;
+        }
+
+        if(command[i]=="pop"){
+            cout << s.top() << endl;
+            s.pop() ;
+        }
+
+        if(command[i]=="size"){
+            cout << s.size() << endl ;
+        }
+
+        if(command[i]=="empty"){
+            if(!s.empty()){
+                cout << "0" << endl ;
+            }
+            else{
+                cout << "1" << endl ;
+            }
+        }
+
+        if(command[i]=="top"){
+            cout << s.top() << endl ;
+        }
+    }
+    // Please write your code here.
+
+    return 0;
+}
