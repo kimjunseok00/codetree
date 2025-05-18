f594af257b19fb6a374308c75ad9c27dfacedbad
kimjunseok00 <proluver1321@gmail.com>
2025-05-18 21:28:52 +0900

    Title: 정수 명령 처리 3, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250518/정수 명령 처리 3/process-numeric-commands-3.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <deque>
+using namespace std;
+
+int N;
+string command[10000];
+int num[10000];
+
+int main() {
+    cin >> N;
+    
+    for (int i = 0; i < N; i++) {
+        cin >> command[i];
+        if (command[i] == "push") {
+            cin >> num[i];
+        }
+    }
+    
+    deque<int> s;
+    
+    for(int i =0 ; i<N ; i++){
+        
+        if(command[i]=="push_front"){
+            s.push_front(num[i]) ;
+        }
+
+        if(command[i]=="pop_front"){
+            cout << s.front() << endl;
+            s.pop_front() ;
+        }
+
+        if(command[i]=="size"){
+            cout << s.size() << endl ;
+        }
+
+        if(command[i]=="empty"){
+            if(!s.empty()){
+                cout << "0" << endl ;
+            }
+            else{
+                cout << "1" << endl ;
+            }
+        }
+        if(command[i]=="front"){
+            cout << s.front() << endl ;
+        }
+        if(command[i]=="back") cout << s.back() << endl; 
+        if(command[i]=="popback") { 
+            cout<< s.back() << endl ;
+            s.pop_back();
+        }
+        if(command[i]=="push_back") {
+        s.push_back();
+        }
+    }
+        
+    return 0;
+}
